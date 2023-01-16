a843e40f4d1719f3f07832bc613abce2425c6d22
Damien Schneider <damien.schneider01@gmail.com>
2023-01-16 23:25:28 +0100

    Create main structure with basics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+// components
+import Home from './pages/home/Home'
+import Create from './pages/create/Create'
+import Recipe from './pages/recipe/Recipe'
+import Search from './pages/search/Search'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/create" component={Create} />
+          <Route path="/recipes/:id" component={Recipe} />
+          <Route path="/search" component={Search} />
+        </Switch>
+      </Router>
     </div>
   );
 }
