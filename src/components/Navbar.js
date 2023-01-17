4eb68e712181c475b318f2a45f1fd096d51fa590
Damien Schneider <damien.schneider01@gmail.com>
2023-01-17 22:46:21 +0100

    Add the search bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import './Navbar.css';
 import { Link } from 'react-router-dom';
-
+//components
+import SearchBar from './SearchBar';
 
 export default function Navbar() {
     return (
@@ -9,6 +10,7 @@ export default function Navbar() {
                 <Link to='/' className="brand">
                     <h1>Poly Cooking</h1>
                 </Link>
+                <SearchBar />
                 <Link to='/create'>
                     Create Recipes
                 </Link>
