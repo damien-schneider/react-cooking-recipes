834028ccfd44a4571a903c518acf10a66c7b843a
Damien Schneider <damien.schneider01@gmail.com>
2023-01-19 01:02:54 +0100

    :construction: Setup firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,3 +1,19 @@
 import firebase from 'firebase/app'
-
 import 'firebase.firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCnoQmf2IxqC2v-4tuwB0w-OLSDZsi0R0w",
+    authDomain: "poly-cooking.firebaseapp.com",
+    projectId: "poly-cooking",
+    storageBucket: "poly-cooking.appspot.com",
+    messagingSenderId: "787647227935",
+    appId: "1:787647227935:web:d61406be7e86d8b8e60d2a"
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+// Initialize services
+const projectFirestore = firebase.firestore()
+
+export { projectFirestore }
